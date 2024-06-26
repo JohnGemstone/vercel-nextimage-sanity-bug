96feef66f6a4c420a929c823cb48325dbb22b110
Jim Johnston <james.johnston.uk@gmail.com>
2024-06-26 22:30:30 +0100

    add notes

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -19,13 +19,16 @@ export default async function Gallery({
   const imgSrcs = imageLibrary.sort(() => Math.random() - 0.5).slice(0, 10);
 
   return (
-    <>
+    <main className="p-5">
       <div>
         <a href={`./${(parseInt(params.slug) - 1) % NUM_PAGES}`}>Previous</a>
         <span> | </span>
         <a href={`./${(parseInt(params.slug) + 1) % NUM_PAGES}`}>Next</a>
       </div>
       <ImageGallery imgSrcs={imgSrcs} />
-    </>
+      <div>
+        Anything <span className="font-bold text-pink-500">pink</span> shows image not loaded.
+      </div>
+    </main>
   );
 }
